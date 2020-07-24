d9813a3e207ba531e0bc101726db97f9f9dabdde
Axel Lopez <lopezaxel@protonmail.com>
2020-07-24 09:11:42 -0300

    Add #promotion and #promote

--- lib/gameboard.rb
+++ lib/gameboard.rb
@@ -11,7 +11,7 @@ class Gameboard
 end
 
 class Pawn
-  attr_reader :gameboard, :color, :position
+  attr_reader :gameboard, :color, :position, :direction
   attr_accessor :initial_position
 
   def initialize(gameboard, color, position)
@@ -19,6 +19,7 @@ class Pawn
     @color = color
     @initial_position = position
     @position = initial_position
+    @direction = pick_direction
   end
 
   def moves(square)
@@ -27,8 +28,8 @@ class Pawn
     row = square[0]
     col = square[1]
 
-    piece_one = gameboard.board[row - 1][col]
-    piece_two = gameboard.board[row - 2][col]
+    piece_one = gameboard.board[row + direction][col]
+    piece_two = gameboard.board[row + direction * 2][col]
 
     check_moves(piece_one, piece_two)
   end
@@ -36,7 +37,7 @@ class Pawn
   def check_moves(piece_one, piece_two)
     if is_a_pawn?(piece_one) && same_color?(piece_one)
       piece_one
-    elsif empty_string?(piece_one) && is_a_pawn?(piece_two) &&
+    elsif is_empty?(piece_one) && is_a_pawn?(piece_two) &&
     same_color?(piece_two) && hasnt_moved?(piece_two)
       piece_two
     else
@@ -50,10 +51,43 @@ class Pawn
     piece_col = move[2]
 
     piece = gameboard.board[piece_row][piece_col]
-    pawn = gameboard.board[piece_row - 1][pawn_col]
+    pawn = gameboard.board[piece_row + direction][pawn_col]
 
-    return false if empty_string?(piece) || same_color?(piece)
+    return false if is_empty?(piece) || same_color?(piece)
     return pawn if is_a_pawn?(pawn) && same_color?(pawn)
+  end
+
+  def promotion(move)
+    row = move[0]
+    col = move[1]
+    piece_letter = move[2]
+
+    if white? && row == gameboard.board.size - 1 || black? && row == 0
+      gameboard.board[row][col] = promote(piece_letter)
+    else
+      false
+    end
+  end
+
+  def promote(letter)
+    case letter
+    when "Q"
+      #Queen.new
+    when "R"
+      #Rook.new
+    when "B"
+      #Bishop.new
+    when "N"
+      #Knight.new
+    end
+  end
+
+  def pick_direction
+    if white?
+      -1
+    elsif black?
+      1
+    end
   end
 
   def hasnt_moved?(pawn)
@@ -68,7 +102,7 @@ class Pawn
     piece.class == Pawn
   end
 
-  def empty_string?(string)
+  def is_empty?(string)
     string == " "
   end
 
@@ -76,6 +110,14 @@ class Pawn
     row = square[0]
     col = square[1]
     gameboard.board[row][col] == " "
+  end
+
+  def white?
+    color == "white"
+  end
+
+  def black?
+    color == "black"
   end
 end
 
--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -10,6 +10,9 @@ describe "Pawn" do
     it "return pawn 1 square below if pawn same color below" do
       gameboard.board[2][2] = pawn_one
       expect(pawn_one.moves([3, 2])).to eql(pawn_one)
+
+      gameboard.board[2][2] = pawn_black
+      expect(pawn_black.moves([1, 2])).to eql(pawn_black)
     end
 
     context "2 squares below pawn same color" do
@@ -17,11 +20,17 @@ describe "Pawn" do
         gameboard.board[2][2] = " "
         gameboard.board[1][2] = pawn_two
         expect(pawn_one.moves([3, 2])).to eql(pawn_two)
+
+        gameboard.board[6][7] = pawn_black
+        expect(pawn_black.moves([4, 7])).to eql(pawn_black)
       end
 
       it "return false if pawn has moved" do
         gameboard.board[1][2].initial_position = [0, 0]
         expect(pawn_one.moves([3, 2])).to eql(false)
+
+        gameboard.board[6][7].initial_position = [7, 7]
+        expect(pawn_one.moves([4, 7])).to eql(false)
       end
     end
     
@@ -29,17 +38,29 @@ describe "Pawn" do
       gameboard.board[1][2] = " "
       gameboard.board[2][2] = " "
       expect(pawn_one.moves([3, 2])).to eql(false)
+
+      gameboard.board[6][7] = " "
+      gameboard.board[5][7] = " "
+      expect(pawn_one.moves([4, 7])).to eql(false)
     end
 
     context "1 square below pawn other color" do
       it "return false" do
         gameboard.board[2][2] = pawn_black
         expect(pawn_one.moves([3, 2])).to eql(false)
+
+        gameboard.board[5][7] = pawn_one
+        expect(pawn_black.moves([4, 7])).to eql(false)
       end
 
       it "return false if 2 squares below pawn same color" do
+        gameboard.board[2][2] = " "
         gameboard.board[1][2] = pawn_two
         expect(pawn_one.moves([3, 2])).to eql(false)
+
+        gameboard.board[5][7] = " "
+        gameboard.board[6][7] = pawn_two
+        expect(pawn_one.moves([4, 7])).to eql(false)
       end
     end
   end
@@ -51,9 +72,13 @@ describe "Pawn" do
 
     context "pawn attacks piece" do
       it "return pawn" do
-        gameboard.board[4][5] = piece
-        gameboard.board[3][4] = pawn
-        expect(pawn.attack_moves([4, 4, 5])).to eql(pawn)
+        gameboard.board[3][3] = piece
+        gameboard.board[4][4] = pawn
+        expect(pawn.attack_moves([4, 3, 3])).to eql(pawn)
+
+        gameboard.board[5][4] = piece
+        gameboard.board[6][5] = pawn
+        expect(piece.attack_moves([4, 6, 5])).to eql(piece)
       end
 
       it "return false if same color" do
@@ -69,4 +94,21 @@ describe "Pawn" do
       end
     end
   end
+
+  describe "#promotion" do
+    gameboard = Gameboard.new
+    black_pawn = Pawn.new(gameboard, "black", [1, 0])
+    white_pawn = Pawn.new(gameboard, "white", [6, 0])
+
+    it "return false if row isnt 7 or 0" do
+      expect(black_pawn.promotion([1, 0, "Q"])).to eql(false)
+      expect(white_pawn.promotion([6, 7, "Q"])).to eql(false)
+    end
+
+    it "doesn't return false if row is 7 or 0" do
+      expect(black_pawn.promotion([0, 0, "R"])).not_to eql(false)
+      expect(white_pawn.promotion([7, 7, "R"])).not_to eql(false)
+    end
+  end
 end
+
