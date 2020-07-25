be5c336f1890cc025c940cae6cc1bba412cd0df1
Axel Lopez <lopezaxel@protonmail.com>
2020-07-25 08:01:13 -0300

    Add Knight class

--- lib/knight.rb
+++ lib/knight.rb
@@ -6,24 +6,66 @@ class Knight < Piece
   end
 
   def moves(move)
-    possible_moves = []
+    moves = possible_moves(move) 
+    knights = possible_knights(moves)
+
+    if knights.length > 1
+      pick_knight(knights, move.last)
+    elsif knights.length == 1
+      knights.first
+    else
+      false
+    end
+  end
+
+  def possible_moves(move)
+    moves = []
     row = move[0]
     col = move[1]
 
-    possible_moves << [row + 1, col + 2]
-    possible_moves << [row + 1, col - 2]
-    possible_moves << [row - 1, col + 2]
-    possible_moves << [row - 1, col - 2]
-    possible_moves << [row + 2, col + 1]
-    possible_moves << [row + 2, col - 1]
-    possible_moves << [row - 2, col + 1]
-    possible_moves << [row - 2, col - 1]
+    moves << [row + 1, col + 2]
+    moves << [row + 1, col - 2]
+    moves << [row - 1, col + 2]
+    moves << [row - 1, col - 2]
+    moves << [row + 2, col + 1]
+    moves << [row + 2, col - 1]
+    moves << [row - 2, col + 1]
+    moves << [row - 2, col - 1]
 
-    possible_moves.sort.keep_if { |m| gameboard.inside_board?(m) }
+    moves.sort.keep_if { |m| gameboard.inside_board?(m) }
+  end
+
+  def possible_knights(moves)
+    knights = []
+
+    moves.each do |move|
+      square = gameboard.board[move[0]][move[1]]
+
+      knights << square if is_a_knight?(square) && same_color?(square)
+    end
+
+    knights
+  end
+
+  def pick_knight(knights, dif)
+    row_or_col = disambiguate(knights)
+    knights.each do |knight|
+      return knight if knight.position[row_or_col] == dif
+    end
+  end
+
+  def disambiguate(positions)
+    knight_1 = positions[0]
+    knight_2 = positions[1]
+
+    knight_1.position[1] == knight_2.position[1] ? 0 : 1
   end
 
   def is_a_knight?(piece)
     piece.class == Knight
   end
 end
+
+require "./lib/gameboard.rb";gameboard = Gameboard.new
+knight = Knight.new(gameboard, "white", [3, 7])
 
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -4,18 +4,91 @@ require './lib/gameboard.rb'
 describe "Knight" do
   describe "#moves" do
     gameboard = Gameboard.new
+    knight_one = Knight.new(gameboard, "white", [3, 7])
+    knight_two = Knight.new(gameboard, "white", [7, 7])
+
+    it "return correct knight" do
+      gameboard.board[3][7] = knight_one
+      expect(knight_one.moves([5, 6])).to eql(knight_one)
+    end
+
+    it "return false when no knight there" do
+      expect(knight_one.moves([7, 0])).to eql(false)
+    end
+
+    context "when 2 knights" do
+      it "return correct knight when same column" do
+        gameboard.board[3][7] = knight_one
+        gameboard.board[7][7] = knight_two
+        expect(knight_one.moves([5, 6, 3])).to eql(knight_one)
+        expect(knight_one.moves([5, 6, 7])).to eql(knight_two)
+      end
+
+      it "return correct knight when same row" do
+        gameboard.board[7][5] = knight_one
+        gameboard.board[7][3] = knight_two
+        knight_one.position = [7, 5]
+        knight_two.position = [7, 3]
+
+        expect(knight_one.moves([5, 4, 5])).to eql(knight_one)
+      end
+    end
+  end
+
+  describe "#possible_moves" do
+    gameboard = Gameboard.new
     knight = Knight.new(gameboard, "white", [0, 1])
 
     it "returns [[1, 2], [2, 1]] when [0, 0]" do
-      moves = knight.moves([0, 0])
+      moves = knight.possible_moves([0, 0])
       expect(moves).to eql([[1, 2], [2, 1]])
     end
 
     it "returns correct moves when [2, 5]" do
-      moves = knight.moves([2, 5])
+      moves = knight.possible_moves([2, 5])
       expected_moves = [[0, 4], [0, 6], [1, 3], [1, 7], [3, 3], [3, 7], 
                        [4, 4], [4, 6]]
       expect(moves).to eql(expected_moves)
+    end
+  end
+
+  describe "#possible_knights" do
+    gameboard = Gameboard.new
+    knight_one = Knight.new(gameboard, "white", [0, 4])
+    knight_two = Knight.new(gameboard, "white", [4, 6])
+
+    it "return knight if knight in moves of [2, 5]" do
+      gameboard.board[0][4] = knight_one
+      moves = knight_one.possible_moves([2, 5])
+      expect(knight_one.possible_knights(moves)).to eql([knight_one])
+    end
+
+    it "return 2 knights if two knight in moves of [2, 5]" do
+      gameboard.board[0][4] = knight_one
+      gameboard.board[4][6] = knight_two
+      moves = knight_one.possible_moves([2, 5])
+      expect(knight_one.possible_knights(moves)).to eql([knight_one, knight_two])
+    end
+  end
+
+  describe "#pick_knight" do
+    gameboard = Gameboard.new
+    knight_one = Knight.new(gameboard, "black", [2, 7])
+    knight_two = Knight.new(gameboard, "black", [6, 7])
+
+
+    context "when 2 knights" do
+      it "return correct knight if same column" do
+        knight = knight_one.pick_knight([knight_one, knight_two], 6)
+        expect(knight).to be(knight_two)
+      end
+
+      it "return correct knight if same row" do
+        knight_one.position = [4, 4]
+        knight_two.position = [4, 6]
+        knight = knight_one.pick_knight([knight_one, knight_two], 6)
+        expect(knight).to be(knight_two)
+      end
     end
   end
 end
